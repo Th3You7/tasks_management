bb0323870d44891bf14478b3ca4b1a6bbca058a0
Th3you7 <elbasiriy@gmail.com>
2024-11-26 00:01:39 +0100

    handling filter, add & display date

--- index.c
+++ index.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct 
 {
@@ -18,6 +19,38 @@ typedef struct
     Date start_date;
 } Task;
 
+// void compare(const void *a, const void *b) {
+//     const Task *date1 = (const Task *)a;
+//     const Task *date2 = (const Task *)b;
+//     if (date1->due_date.year != date2->due_date.year) {
+//         return date1->due_date.year - date2->due_date.year;
+//     } else if (date1->due_date.month != date2->due_date.month) {
+//         return date1->due_date.month - date2->due_date.month;
+//     } else {
+//         return date1->due_date.day - date2->due_date.day;
+//     }
+// }
+
+// void order(Task tasks[], int count) {
+
+//     int option;
+//     if(!count){ 
+//         printf("*** No tasks, try to add one first ***");
+//         return;
+//     }
+//    do
+//    {
+//     printf("Sort (1 ascending, 2 descending): ");
+//     scanf("%d", &option);
+//    } while (!option && option != 1);
+   
+//    qsort(tasks, count, sizeof(tasks[0].due_date), compare);
+//    for (int i = 0; i < count; i++)
+//    {
+//     printf("%s - %d/%d/%d", tasks[i].title, tasks[i].due_date.day, tasks[i].due_date.month, tasks[i].due_date.year);
+//    }
+   
+// }
 
 void add_title(Task* task) {
     char title[50];
@@ -62,6 +95,32 @@ void add_priority(Task* task) {
     
 }
 
+void add_date(Task* task){
+    int day, month, year;
+    printf("=> Date \n");
+    do
+    {
+        printf("==>Month: \n");
+        scanf("%d", &month);
+        if(month > 12 || month < 1)printf("*** Invalid input ***\n");
+    } while (month > 12 || month < 1 );
+
+    do
+    {
+        printf("==>Day: \n");
+        scanf("%d", &day);
+        if(day < 1 || (month == 2 && day > 28) || (month != 2 && day > 31)) printf("*** Invalid input ***\n");
+    } while (day < 1 || (month == 2 && day > 28) || (month != 2 && day > 31));
+
+    do
+    {
+        printf("==>Year( > 2010): \n");
+        scanf("%d", &year);
+        if(year < 2010) printf("*** Invalid input ***\n");
+    } while (year < 2010);
+    
+}
+
 void add_task(Task tasks[], int *count) {
     Task new_task;
     printf("*** Please add details ***\n");
@@ -70,6 +129,7 @@ void add_task(Task tasks[], int *count) {
     add_desc(&new_task);
     add_status(&new_task);
     add_priority(&new_task);
+    add_date(&new_task);
     tasks[*count] = new_task;
     *count += 1;
     printf("\n***** Your task is added successfully!! *****\n");
@@ -105,50 +165,87 @@ void list_task(Task DB[], int count) {
             printf("==> Description: %s\n", DB[count-1].desc);
             printf("==> Status: %s\n", DB[count-1].status ? "COMPLETED" : "INCOMPLETED");
             printf("==> Priority: %s\n", DB[count-1].priority ? "HIGH" : "LOW");
+            printf("==> Date: %d/%d/%d",  DB[count-1].due_date.day,  DB[count-1].due_date.month,  DB[count-1].due_date.year);
         }
     } while (task > count || task <= 0);
 }
 
 void update_task(Task DB[], int count) {
     int task;
-    int update_title, update_desc, update_priority, update_status;
+    int update_title, update_desc, update_priority, update_status, update_date;
     if(!count) {
         printf("*** No tasks to edit ***"); 
         return;
     }
-    printf("Please select the task you want to update\n");
-    //list all tasks
-    list_all(DB, count);
-    //which task to edit
-    scanf("%d", &task);
+   
+    do
+    {
+         //list all tasks
+        list_all(DB, count);
+        printf("Please select the task you want to update from above\n");
+        //which task to edit
+        scanf("%d", &task);
+    } while (!task || task < 0 || task > count);
+    
 
     //ask for title
-    printf("Title: %s => update ? (Yes: 0, No: 1)\n", DB[task-1].title);
-    scanf("%d", &update_title);
+    do
+    {
+       printf("Title: %s => update ? (Yes: 0, No: 1)\n", DB[task-1].title);
+       scanf("%d", &update_title);
+       if(!update_title && update_title > 1) printf("*** Invalid option ***");
+    } while (!update_title && update_title > 1);
+    //if yes
     if(!update_title){
         getchar();
         add_title(&DB[task-1]);
     }
     //ask for desc
-    printf("Description: %s => update ? (Yes: 0, No: 1) ",  DB[task-1].desc);
-    scanf("%d", &update_desc);
+    do
+    {
+        printf("Description: %s => update ? (Yes: 0, No: 1) ",  DB[task-1].desc);
+        scanf("%d", &update_desc);
+       if(!update_desc && update_desc > 1) printf("*** Invalid option ***");
+    } while (!update_desc && update_desc > 1);
+    //if yes
     if(!update_desc){
         getchar();
         add_desc(&DB[task-1]);
     }
-    //ask for priority
-    printf("Priority: %s => update ? (Yes: 0, No: 1) ",  DB[task-1].priority ? "HIGH" : "LOW");
-    scanf("%d", &update_priority);
+    //ask for property
+    do
+    {
+        printf("Priority: %s => update ? (Yes: 0, No: 1) ",  DB[task-1].priority ? "HIGH" : "LOW");
+        scanf("%d", &update_priority);
+        if(!update_priority && update_priority > 1) printf("*** Invalid option ***");
+    } while (!update_priority && update_priority > 1);
+    //if yes
     if(!update_priority){
         add_priority(&DB[task-1]);
     }
     //ask for status
-    printf("Status: %s => update ? (Yes: 0, No: 1) ", DB[count-1].status ? "COMPLETED" : "INCOMPLETED");
-    scanf("%d", &update_status);
+    do
+    {
+        printf("Status: %s => update ? (Yes: 0, No: 1) ", DB[count-1].status ? "COMPLETED" : "INCOMPLETED");
+        scanf("%d", &update_status);
+        if(!update_status && update_status);
+    } while (!update_status && update_status);
+    //if yes
     if(!update_status) {
         add_status(&DB[task-1]);
     }
-
+    //ask for date
+    do
+    {
+        printf("Date: %d/%d/%d => update ? (Yes: 0, No: 1)", DB[count-1].due_date.day, DB[count-1].due_date.month, DB[count-1].due_date.year);
+        scanf("%d", &update_date);
+        if(!update_date && update_date);
+    } while (!update_date && update_date);
+    //If yes
+    if(!update_date) {
+        add_date(&DB[task-1]);
+    }
+    
     printf("*** Task updated successfully *** \n");
 }
 
@@ -175,7 +272,34 @@ void delete_task(Task DB[], int *count) {
     }
 }
 
+void filter(Task DB[], int count) {
+    int by, which;
+    
+    do
+    {
+         printf("Filter by => (0 status, 1 priority)");
+        scanf("%d", &by);
+        if(!by && by > 1) printf("*** Invalid option ***");
+    } while (!by && by > 1);
+    
+    do
+    {
+        if(!by) printf("0 INCOMPLETE / 1 COMPLETE");
+        else printf("0 LOW / 1 HIGH");
+         scanf("%d", &which);
+        if(!which && which > 1)  printf("*** Invalid option ***");
+    } while (!which && which > 1);
 
+    for (int i = 0; i < count; i++)
+    {
+        if (!by && DB[i].status == which) printf("==> %d - %s (%s)\n", i + 1, DB[i].title, DB[i].status ? "COMPLETED" : "INCOMPLETED");
+        if(by && DB[i].priority == which) printf("==> %d - %s (%s)\n", i + 1, DB[i].title, DB[i].priority ? "HIGH" : "LOW");
+        
+    }
+    
+    
+    
+}
 
 int main() {
     Task DB[50];
@@ -185,7 +309,7 @@ int main() {
     do
     {
         printf("\n*** Choose an action ***\n");
-        printf("1 - ADD / 2 - LIST / 3 - DETAILS / 4 - UPDATE / 5 - DELETE / 0 - QUIT\n");
+        printf("1 - ADD / 2 - LIST / 3 - DETAILS / 4 - UPDATE / 5 - DELETE / 6 - FILTER / 7 - Sort / 0 - QUIT\n");
         scanf("%d", &action);
        //Action 1 for adding, 2 for listing, 3 for updating, 4 for deleting, 0 to quit
        switch (action)
@@ -205,6 +329,12 @@ int main() {
        case 5:
         delete_task(DB, &tasks);
         break;  
+       case 6:
+        filter(DB, tasks);
+        break;
+    //    case 7: 
+    //     order(DB, tasks);
+    //     break;
        case 0: 
         printf("**GOODBYE!!**");
         break; 
