3068dcb15397544efbc2f54e66250bd67eee79d1
Th3you7 <elbasiriy@gmail.com>
2024-11-26 11:58:37 +0100

    handle save in file, start order

--- index.c
+++ index.c
@@ -19,38 +19,50 @@ typedef struct
     Date start_date;
 } Task;
 
-// void compare(const void *a, const void *b) {
-//     const Task *date1 = (const Task *)a;
-//     const Task *date2 = (const Task *)b;
-//     if (date1->due_date.year != date2->due_date.year) {
-//         return date1->due_date.year - date2->due_date.year;
-//     } else if (date1->due_date.month != date2->due_date.month) {
-//         return date1->due_date.month - date2->due_date.month;
-//     } else {
-//         return date1->due_date.day - date2->due_date.day;
-//     }
-// }
+void compare_asc(const void *a, const void *b) {
+    const Task *date1 = (const Task *)a;
+    const Task *date2 = (const Task *)b;
+    if (date1->due_date.year != date2->due_date.year) {
+        return date1->due_date.year - date2->due_date.year;
+    } else if (date1->due_date.month != date2->due_date.month) {
+        return date1->due_date.month - date2->due_date.month;
+    } else {
+        return date1->due_date.day - date2->due_date.day;
+    }
+}
 
-// void order(Task tasks[], int count) {
+void compare_dec(const void *a, const void *b) {
+    const Task *date1 = (const Task *)a;
+    const Task *date2 = (const Task *)b;
+    if (date1->due_date.year != date2->due_date.year) {
+        return date2->due_date.year - date1->due_date.year;
+    } else if (date1->due_date.month != date2->due_date.month) {
+        return date2->due_date.month - date1->due_date.month;
+    } else {
+        return date2->due_date.day - date1->due_date.day;
+    }
+}
 
-//     int option;
-//     if(!count){ 
-//         printf("*** No tasks, try to add one first ***");
-//         return;
-//     }
-//    do
-//    {
-//     printf("Sort (1 ascending, 2 descending): ");
-//     scanf("%d", &option);
-//    } while (!option && option != 1);
+void order_task(Task tasks[], int count) {
+
+    int option;
+    if(!count){ 
+        printf("*** No tasks, try to add one first ***");
+        return;
+    }
+   do
+   {
+    printf("Sort (0 ascending, 1 descending): ");
+    scanf("%d", &option);
+   } while (option && option != 1);
    
-//    qsort(tasks, count, sizeof(tasks[0].due_date), compare);
-//    for (int i = 0; i < count; i++)
-//    {
-//     printf("%s - %d/%d/%d", tasks[i].title, tasks[i].due_date.day, tasks[i].due_date.month, tasks[i].due_date.year);
-//    }
+   qsort(tasks, count, sizeof(tasks[0].due_date), option ? compare_asc : compare_dec);
+   for (int i = 0; i < count; i++)
+   {
+    printf("%d- %s (%d/%d/%d) \n", i+ 1, tasks[i].title, tasks[i].due_date.day, tasks[i].due_date.month, tasks[i].due_date.year);
+   }
    
-// }
+}
 
 void add_title(Task* task) {
     char title[50];
@@ -100,24 +112,25 @@ void add_date(Task* task){
     printf("=> Date \n");
     do
     {
-        printf("==>Month: \n");
+        printf("==> Month: ");
         scanf("%d", &month);
         if(month > 12 || month < 1)printf("*** Invalid input ***\n");
     } while (month > 12 || month < 1 );
-
+    task->due_date.month = month;
     do
     {
-        printf("==>Day: \n");
+        printf("==> Day: ");
         scanf("%d", &day);
         if(day < 1 || (month == 2 && day > 28) || (month != 2 && day > 31)) printf("*** Invalid input ***\n");
     } while (day < 1 || (month == 2 && day > 28) || (month != 2 && day > 31));
-
+    task->due_date.day = day;
     do
     {
-        printf("==>Year( > 2010): \n");
+        printf("==> Year(>2010): ");
         scanf("%d", &year);
         if(year < 2010) printf("*** Invalid input ***\n");
     } while (year < 2010);
+    task->due_date.year = year;
     
 }
 
@@ -156,7 +169,7 @@ void list_task(Task DB[], int count) {
     list_all(DB, count);
     do
     {
-        printf("==> Enter a task number:  \n");
+        printf("==>  Please select the task you want to see from above:  \n");
         scanf("%d", &task);
         if (task > count || task <= 0){
              printf("*** Invalid choice ***\n");
@@ -182,7 +195,7 @@ void update_task(Task DB[], int count) {
     {
          //list all tasks
         list_all(DB, count);
-        printf("Please select the task you want to update from above\n");
+        printf("=> Please select the task you want to update from above: ");
         //which task to edit
         scanf("%d", &task);
     } while (!task || task < 0 || task > count);
@@ -256,7 +269,7 @@ void delete_task(Task DB[], int *count) {
          printf("**** All tasks ****\n");
          list_all(DB, *count);
         do{
-            printf("==> Enter a task number:  \n");
+            printf("==>  Please select the task you want to delete from above::  \n");
             scanf("%d", &task);
             if (task > *count || task <= 0){
                 printf("*** Invalid choice ***\n");
@@ -272,12 +285,12 @@ void delete_task(Task DB[], int *count) {
     }
 }
 
-void filter(Task DB[], int count) {
+void filter_task(Task DB[], int count) {
     int by, which;
     
     do
     {
-         printf("Filter by => (0 status, 1 priority)");
+         printf("Filter by => (0 status, 1 priority): ");
         scanf("%d", &by);
         if(!by && by > 1) printf("*** Invalid option ***");
     } while (!by && by > 1);
@@ -301,6 +314,23 @@ void filter(Task DB[], int count) {
     
 }
 
+void save_task(Task DB[], int count) {
+    if(!count) {
+        printf("*** No tasks to save ***"); 
+        return;
+    }
+    FILE *file = fopen("tasks.txt", "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    for(int i = 0; i < count; i++){
+        fprintf(file,"%d - %s(%d/%d/%d)=> %s - %s\n %s\n\n", i + 1, DB[i].title, DB[i].due_date.day, DB[i].due_date.month, DB[i].due_date.year, DB[i].status ? "COMPLETE" : "COMPLETED", DB[i].priority ? "High" : "LOW", DB[i].desc);
+    }
+    fclose(file);
+    printf("*** Tasks Saved successfully ***");
+}
+
 int main() {
     Task DB[50];
     int tasks = 0, action; // tasks to track the number of tasks; action for user action
@@ -309,7 +339,8 @@ int main() {
     do
     {
         printf("\n*** Choose an action ***\n");
-        printf("1 - ADD / 2 - LIST / 3 - DETAILS / 4 - UPDATE / 5 - DELETE / 6 - FILTER / 7 - Sort / 0 - QUIT\n");
+        printf("******** MENU ********\n");
+        printf("1 - ADD / 2 - LIST / 3 - FILTER / 4 - DETAILS / 5 - UPDATE / 6 - DELETE / 7 - SAVE / 0 - QUIT\n");
         scanf("%d", &action);
        //Action 1 for adding, 2 for listing, 3 for updating, 4 for deleting, 0 to quit
        switch (action)
@@ -321,20 +352,23 @@ int main() {
         list_all(DB, tasks);
         break;
        case 3:
+        filter_task(DB, tasks);
+        break; 
+        case 4:
         list_task(DB, tasks);
         break;
-       case 4:
+       case 5:
         update_task(DB, tasks); 
         break;
-       case 5:
-        delete_task(DB, &tasks);
-        break;  
        case 6:
-        filter(DB, tasks);
+        delete_task(DB, &tasks);
         break;
-    //    case 7: 
-    //     order(DB, tasks);
-    //     break;
+       case 7:
+        save_task(DB, tasks);
+        break;
+       case 8:
+        order_task(DB, tasks);
+        break;
        case 0: 
         printf("**GOODBYE!!**");
         break; 
@@ -343,7 +377,5 @@ int main() {
        }
     } while (action);
     
-
-
     return 0;
 }
